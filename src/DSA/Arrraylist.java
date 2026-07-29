ee1a6bdf41a59acc4105493633f4d9a07cda0bbf
Akash Kumar <Akashone15@gmail.com>
2026-07-29 21:57:09 +0530

    collection framework

---
+++ src/DSA/Arrraylist.java
@@ -0,0 +1,12 @@
+package DSA;
+import java.util.ArrayList;
+public class Arrraylist {
+    public static void main(String args[]){
+        ArrayList<Integer>ali=new ArrayList<Integer>();
+        ali.add(10);
+        ali.add(20);
+        ali.add(30);
+        ali.remove(1);
+        System.out.println(ali);
+    }
+}
