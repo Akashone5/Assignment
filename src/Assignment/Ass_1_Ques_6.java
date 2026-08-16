69aec8c794b00c4320b7530de13fd104492702b3
Akash Kumar <Akashone15@gmail.com>
2026-08-16 12:08:45 +0530

    Two sum

---
+++ src/Assignment/Ass_1_Ques_6.java
@@ -0,0 +1,30 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Ass_1_Ques_6 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+
+            int target = sc.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        System.out.println(i + " " + j);
+                        return;
+                    }
+                }
+            }
+
+            System.out.println("-1 -1");
+        }
+    }
